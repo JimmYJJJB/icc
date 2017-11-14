1afc2edf9288091d0eff8aa1a9d87766e508d101
JimmYJJJB <33643292+JimmYJJJB@users.noreply.github.com>
2017-11-13 20:16:18 -0500

    Add files via upload

---
+++ jim/block.py
@@ -0,0 +1,8 @@
+a = float(input("ingrese el numero: "))
+b = int(input("ingrese el siguiente numero: "))
+c = a/b
+d = a//b
+if c==d :
+    print ( str(int(a)) + " es multiplo de " + str(b))
+else:
+    print ( str(int(a)) + " no es multiplo de " + str(b))
---
+++ jim/block2.py
@@ -0,0 +1,11 @@
+a = input("")
+
+if str(a)== "m":
+    em = int(input(""))
+    pm = (220-em)/10
+    print( str(pm))
+if  str(a)== str(m):
+    eh = int(input(""))
+    ph = (210-eh)/10
+    print( str(ph))
+else str(a) =! ("m" or "h") :
